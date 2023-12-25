d34dc85e62826ca4ff13aef7b56cf0ff7c145a42
simderella <146172523+simderella@users.noreply.github.com>
2023-12-26 08:56:57 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 64 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,11 @@
+namespace CodeKata
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine(x - 543);
+        }
+    }
+}

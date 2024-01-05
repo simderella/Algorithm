60efee4437944db27674efc01e5215be71e12616
simderella <146172523+simderella@users.noreply.github.com>
2024-01-05 10:32:11 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 64 ms, Memory: 5460 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cs
@@ -0,0 +1,11 @@
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string input = Console.ReadLine(); 
+            Console.WriteLine(input.Length);
+        }
+    }
+}
